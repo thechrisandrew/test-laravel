002330de6ee583185f992ec0b8f263e60ab026b9
thechrisandrew <67407803+thechrisandrew@users.noreply.github.com>
2022-10-29 23:12:03 +0700

    fix: Make a post button appearing for guest users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,28 +26,36 @@
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <div class="d-flex justify-content-between align-items-center">
-                    <button class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">{{ __('Make a post') }}</button>
-                    <form method="POST" action="/add-post"
-                    enctype="multipart/form-data">
-                    @csrf
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    @guest
+                    @else
+                        <button class="btn btn-outline-secondary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">{{ __('Make a post') }}</button>
+                    @endguest
+                    <form method="POST" action="/add-post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
                             <div class="modal-dialog">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <h1 class="modal-title fs-5" id="exampleModalLabel">{{ __('Create a post') }}</h1>
-                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ __('Create a post') }}
+                                        </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="form-floating">
+                                            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"
+                                                name="text"></textarea>
+                                            <label for="floatingTextarea2">{{ __('Share your thoughts') }}</label>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">{{ __('Cancel') }}</button>
+                                        <button type="submit" class="btn btn-primary">{{ __('Post') }}</button>
+                                    </div>
                                 </div>
-                                <div class="modal-body">
-                                    <div class="form-floating">
-                                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="text"></textarea>
-                                        <label for="floatingTextarea2">{{ __('Share your thoughts') }}</label>
-                                      </div>
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Cancel') }}</button>
-                                  <button type="submit" class="btn btn-primary">{{ __('Post') }}</button>
-                                </div>
-                              </div>
                             </div>
                         </div>
                     </form>
@@ -91,7 +99,8 @@
                                             {{ __('Logout') }}
                                         </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
                                             @csrf
                                         </form>
                                     </div>
